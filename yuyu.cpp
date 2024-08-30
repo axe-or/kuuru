cf42220d2bc09cd7a12f73209a3cce396f9b9a6d
axe-or <marcosfehlauer@gmail.com>
2024-08-30 10:22:29 -0300

    Update yuyu.cpp

--- yuyu.cpp
+++ yuyu.cpp
@@ -32,6 +32,12 @@ using cstring = char const *;
 static_assert(sizeof(isize) == sizeof(usize), "Mismatched size types");
 static_assert(sizeof(f32) == 4 && sizeof(f64) == 8, "Bad floating point size types");
 
+template<typename A, typename B = A>
+struct pair {
+	A a;
+	B b;
+};
+
 template<typename T>
 T min(T a, T b){
 	return (a < b) ? a : b;
@@ -262,6 +268,13 @@ public:
 		return data[idx];
 	}
 
+	T at_or(isize idx, T const& val) const {
+		if(idx < 0 && idx >= length){
+			return val;
+		}
+		return data[idx];
+	}
+	
 	Slice<T> sub(isize start, isize end){
 		assert(start <= end && start >= 0 && end >= 0, "Invalid range for slice");
 		return Slice<T>::from(&data[start], end - start);
@@ -734,9 +747,114 @@ struct string {
 		return s;
 	}
 };
+ 
+constexpr auto surrogate_range = Array<rune, 2>{0xd800, 0xdfff};
+constexpr auto continuation_range = Array<byte, 2>{0x80, 0xbf};
 
-rune utf8_decode(Slice<byte> b){
+constexpr rune utf8_error = 0xfffd;
+
+constexpr rune utf8_range1 = 0x7f;
+constexpr rune utf8_range2 = 0x7ff;
+constexpr rune utf8_range3 = 0xffff;
+constexpr rune utf8_range4 = 0x10ffff;
+
+constexpr rune mask_2 = 0x1f; /* 0001_1111 */
+constexpr rune mask_3 = 0x0f; /* 0000_1111 */
+constexpr rune mask_4 = 0x07; /* 0000_0111 */
+
+constexpr rune mask_x = 0x3f; /* 0011_1111 */
+
+constexpr rune size_2 = 0xc0; /* 110x_xxxx */
+constexpr rune size_3 = 0xe0; /* 1110_xxxx */
+constexpr rune size_4 = 0xf0; /* 1111_0xxx */
+
+constexpr rune cont = 0x80; /* 10xx_xxxx */
+
+pair<Array<byte, 4>, i32> utf8_encode(rune r){
+	// TODO: Prevent invalid codepoints
+	int n = 0;
+	Array<byte, 4> bytes = {0};
+	
+	if(r <= utf8_range1){
+		n = 1;
+		bytes[0] = r;
+	}
+	else if(r <= utf8_range2){
+		n = 2;
+		bytes[0] = size_2 | ((r >> 6) & mask_2);
+		bytes[1] = cont   | ((r >> 0) & mask_x);
+	}
+	else if(r <= utf8_range3){
+		n = 3;
+		bytes[0] = size_3 | ((r >> 12) & mask_3);
+		bytes[1] = cont   | ((r >> 6) & mask_x);
+		bytes[2] = cont   | ((r >> 0) & mask_x);
+	}
+	else if(r <= utf8_range4){
+		n = 4;
+		bytes[0] = size_4 | ((r >> 18) & mask_4);
+		bytes[1] = cont   | ((r >> 12) & mask_x);
+		bytes[2] = cont   | ((r >> 6)  & mask_x);
+		bytes[3] = cont   | ((r >> 0)  & mask_x);
+	}
+	return res;
 }
+
+pair<rune, i32> utf8_decode(Slice<byte> s){
+	constexpr pair<rune, i32> decode_error = {utf8_error, 0};
+	if(s.empty()){ return decode_error; }
+
+	auto buf = s.raw_data();
+	u8 first = buf[0];
+	
+	i32 len = 0;
+	rune codepoint = 0;
+
+	if((first & cont) == 0){
+		len = 1;
+		codepoint |= first;
+	}
+	else if ((first & ~mask_2) == size_2 && s.size() >= 2){
+		len = 2;
+		codepoint |= (buf[0] & mask_2) << 6;
+		codepoint |= (buf[1] & mask_x) << 0;
+	}
+	else if ((first & ~mask_3) == size_3 && s.size() >= 2){
+		len = 3;
+		codepoint |= (buf[0] & mask_3) << 12;
+		codepoint |= (buf[1] & mask_x) << 6;
+		codepoint |= (buf[2] & mask_x) << 0;
+	}
+	else if ((first & ~mask_4) == size_4 && s.size() >= 2){
+		len = 4;
+		codepoint |= (buf[0] & mask_4) << 18;
+		codepoint |= (buf[1] & mask_x) << 12;
+		codepoint |= (buf[2] & mask_x) << 6;
+		codepoint |= (buf[3] & mask_x) << 0;
+	}
+	else {
+		return decode_error;
+	}
+
+	// Validation
+	if(codepoint >= surrogate_range[0] && codepoint <= surrogate_range[1]){
+		return decode_error;
+	}
+	if(len > 1 && (buf[1] < continuation_range[0] || buf[1] > continuation_range[1])){
+		return decode_error;
+	}
+	if(len > 2 && (buf[2] < continuation_range[0] || buf[2] > continuation_range[1])){
+		return decode_error;
+	}
+	if(len > 3 && (buf[3] < continuation_range[0] || buf[3] > continuation_range[1])){
+		return decode_error;
+	}
+
+	return {codepoint, len};
+}
+
+
+
 #pragma endregion
 
 int main(void) {
