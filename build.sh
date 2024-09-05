805188cca91159be0ab729d8eecf8f8616693171
axe-or <marcosfehlauer@gmail.com>
2024-09-05 11:30:13 +0000

    strip binary

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cc='clang++'
 cflags='-std=c++17 -O1 -fPIC -Wall -Wextra'
 ldflags=''
 
+set -x
 
-$cc $cflags -c base/_lib.cpp -o base.o
-
+$cc $cflags -c base/lib.cpp -o base.o
 $cc $cflags yuyu.cpp base.o -o yuyu $ldflags
