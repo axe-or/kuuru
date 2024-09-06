213bfdd0f8ae5aec8cce7890ba6f2836ee8fe3ef
axe-or <marcosfehlauer@gmail.com>
2024-09-06 16:33:19 +0000

    lexer

--- lexer.cpp
+++ lexer.cpp
@@ -19,11 +19,14 @@ bool Lexer::advance_on_match(rune expect){
     return true;
 }
 
-#define TOKEN2(RUNE_, T_, F_) \
-    if(advance_on_match(RUNE_)){ \
+#define TOKEN1(T_) \
+    { tk.kind = T_; } break
+
+#define TOKEN2(R_, T_, ALT_) \
+    if(advance_on_match(R_)){ \
         tk.kind = T_; \
     } else { \
-        tk.kind = F_; \
+        tk.kind = ALT_; \
     } break
 
 #define TOKEN3(R0_, T0_, R1_, T1_, ALT_) \
@@ -43,24 +46,40 @@ Token Lexer::next(){
     auto [r, n] = advance();
     if(r == 0){ return Token{ .kind = K::End_Of_File };}
 
-
     switch(r){
-        case '(': {
-            tk.kind = TokenKind::Paren_Open;
-        } break;
+        case '(': TOKEN1(K::Paren_Open);
+        case ')': TOKEN1(K::Paren_Close);
+        case '[': TOKEN1(K::Square_Open);
+        case ']': TOKEN1(K::Square_Close);
+        case '{': TOKEN1(K::Curly_Open);
+        case '}': TOKEN1(K::Curly_Close);
 
+        case '.': TOKEN1(K::Dot);
+        case ',': TOKEN1(K::Comma);
+        case ':': TOKEN1(K::Colon);
+        case ';': TOKEN1(K::Semicolon);
+        case '^': TOKEN1(K::Caret);
+
+        case '!': TOKEN2('=', K::Not_Eq, K::Logic_Not);
         case '=': TOKEN2('=', K::Eq_Eq, K::Equal);
         case '>': TOKEN2('=', K::Gte, K::Gt);
         case '<': TOKEN2('=', K::Lte, K::Lt);
 
-        case '&': TOKEN2('&', K::Logic_And, K::And);
-
+        case '+': TOKEN2('=', K::Plus_Assign, K::Plus);
+        case '-': TOKEN2('=', K::Minus_Assign, K::Minus);
+        case '*': TOKEN2('=', K::Star_Assign, K::Star);
+        case '/': TOKEN2('=', K::Slash_Assign, K::Slash);
+        case '%': TOKEN2('=', K::Modulo_Assign, K::Modulo);
+         
+        case '&': TOKEN3('&', K::Logic_And, '=', K::And_Assign, K::And);
         case '|': TOKEN3('|', K::Logic_Or, '=', K::Or_Assign, K::Or);
+        case '~': TOKEN2('=', K::Xor_Assign, K::Xor);
     }
 
     return tk;
 }
 
+#undef TOKEN1
 #undef TOKEN2
 #undef TOKEN3
 
