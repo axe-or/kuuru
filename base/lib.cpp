81ca643fece8d10868ddec8fcde451d6dde01118
axe-or <marcosfehlauer@gmail.com>
2024-09-09 12:39:09 +0000

    I'm never using submodules again

--- base/lib.cpp
+++
@@ -1,9 +0,0 @@
-#include "base.hpp"
-
-#include "arena_allocator.cpp"
-#include "assert.cpp"
-#include "base.hpp"
-#include "heap_allocator.cpp"
-#include "memory.cpp"
-#include "string.cpp"
-#include "utf8.cpp"
